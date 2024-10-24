959e9d03c9c761a54d25e0b60c7e2a142651c81c
Vusal <vusal.novruzov.1@iliauni.edu.ge>
2024-10-24 20:47:58 +0400

    adding landing page

--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -3,8 +3,15 @@ import { PropsWithChildren } from "react";
 
 export default function Guest({ children }: PropsWithChildren) {
   return (
-    <div className="flex flex-col items-center bg-gray-100 pt-6 mt-48 sm:pt-0 px-2">
-      <div></div>
+    <div className="flex flex-col items-center bg-slate-100 pt-6 mt-48 sm:pt-0 px-2">
+      <div>
+        <Link href="/dashboard" className="flex gap-3">
+          <img src="/logo.svg" className="w-10 h-10" alt="logo" />
+          <h2 className="font-extrabold text-3xl text-primary mb-5">
+            AIdeaForge
+          </h2>
+        </Link>
+      </div>
 
       <div className="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
         {children}
--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -5,11 +5,48 @@ export default function Welcome() {
   return (
     <>
       <Head title="Welcome" />
-      <div className="flex flex-col items-center justify-center text-7xl h-screen font-extrabold uppercase ">
-        <h1>Landing Page!</h1>
-        <Link href="/dashboard">
-          <PrimaryButton className="bg-primary">Get started</PrimaryButton>
-        </Link>
+
+      <div className="h-screen bg-slate-100">
+        <div className="max-w-7xl mx-auto flex  justify-between p-4">
+          <Link href="/dashboard" className="flex gap-3">
+            <img
+              src="/logo.svg"
+              className="w-7 h-7 lg:w-10 lg:h-10"
+              alt="logo"
+            />
+            <h2 className="font-extrabold text-xl lg:text-3xl text-primary mb-10">
+              AIdeaForge
+            </h2>
+          </Link>
+
+          <Link href="/dashboard">
+            <PrimaryButton
+              className=" 
+              inline-flex justify-center items-center  gap-x-3 text-center bg-gradient-to-tl from-blue-600 to-violet-600 hover:from-violet-600 hover:to-blue-600 border border-transparent text-white text-sm font-medium rounded-md focus:outline-none focus:ring-1 focus:ring-gray-600 py-2 px-2 lg:py-3 lg:px-4 dark:focus:ring-offset-gray-800
+              "
+            >
+              Dashboard
+            </PrimaryButton>
+          </Link>
+        </div>
+        <div className="max-w-[85rem] mx-auto flex flex-col items-center justify-center mt-32 lg:mt-56 px-8">
+          <h1 className="text-5xl lg:text-6xl text-gray-800 mb-5 font-bold text-center">
+            AI Content{" "}
+            <span className="bg-clip-text bg-gradient-to-tl from-blue-600 to-violet-600 text-transparent">
+              Generator
+            </span>
+          </h1>
+
+          <p className=" text-md lg:text-lg text-gray-600 dark:text-neutral-400 text-center w-full max-w-[700px] mb-5">
+            Effortlessly Create content, optimize SEO, and enhance your writing
+            and code—all powered by AI.
+          </p>
+          <Link href="/dashboard">
+            <PrimaryButton className="inline-flex justify-center items-center  gap-x-3 text-center bg-gradient-to-tl from-blue-600 to-violet-600 hover:from-violet-600 hover:to-blue-600 border border-transparent text-white text-sm font-medium rounded-md focus:outline-none focus:ring-1 focus:ring-gray-600 py-3 px-4 dark:focus:ring-offset-gray-800">
+              Get started
+            </PrimaryButton>
+          </Link>
+        </div>
       </div>
     </>
   );
